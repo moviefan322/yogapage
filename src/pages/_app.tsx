7098d61bd26dbd8f1868cc8835d88d2260aa20c3
moviefan322 <philman202@yahoo.com>
2023-12-05 15:51:06 -0500

    adds custom colors to sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@/styles/bootstrap.scss";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import Layout from "@/components/layout/layout";
+import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
