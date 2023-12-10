7f86398f79c911d3dd111f9f04da9d5836099c55
moviefan322 <philman202@yahoo.com>
2023-12-10 16:33:31 -0500

    adds blank calendar to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
 
 const contact = () => {
+  
+  
   return (
     <div>
       <h1>CONTACT</h1>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          { title: "event 1", date: "2023-12-01" },
+          { title: "event 32", date: "2023-12-02" },
+          { title: "event 2", date: "2023-12-02" },
+        ]}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default contact
+export default contact;
