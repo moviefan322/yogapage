c31c5e369f8611ae35ddf953a72f998200afc356
moviefan322 <philman202@yahoo.com>
2023-12-12 01:26:11 -0500

    outlines services page

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -6,23 +6,113 @@ const services = () => {
     <div>
       <h1 className="text-center">SERVICES</h1>
       <div className="d-flex align-items-center px-2 mx-1">
-        <div className="col-md-8">
-          <h2>Acupressure</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
-            tempore.
-          </p>
-        </div>
-        <div className="col-md-2 p-1">
-          <Image
-            src="/acupress.jpeg"
-            alt="hand massage"
-            width={100}
-            height={100}
-          />
+        <div className="col-md-10 d-flex align-items-center border border-2 border-dark px-1">
+          <div className="col-md-8">
+            <h2>Acupressure</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              tempore.
+            </p>
+          </div>
+          <div className="col-md-2 p-1 image1">
+            <Image
+              src="/acupress.jpeg"
+              alt="hand massage"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
         <div className="col-md-2">
-          <button>Book Appt</button>
+          <button>Book Now</button>
+        </div>
+      </div>
+      <div className="d-flex align-items-center px-2 mx-1">
+        <div className="col-md-10 d-flex align-items-center border border-2 border-dark px-1">
+          <div className="col-md-8">
+            <h2>Cupping</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              tempore.
+            </p>
+          </div>
+          <div className="col-md-2 p-1 image1">
+            <Image
+              src="/acupress.jpeg"
+              alt="hand massage"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="col-md-2">
+          <button>Book Now</button>
+        </div>
+      </div>
+      <div className="d-flex align-items-center px-2 mx-1">
+        <div className="col-md-10 d-flex align-items-center border border-2 border-dark px-1">
+          <div className="col-md-8">
+            <h2>Accu x cupping</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              tempore.
+            </p>
+          </div>
+          <div className="col-md-2 p-1 image1">
+            <Image
+              src="/acupress.jpeg"
+              alt="hand massage"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="col-md-2">
+          <button>Book Now</button>
+        </div>
+      </div>
+      <div className="d-flex align-items-center px-2 mx-1">
+        <div className="col-md-10 d-flex align-items-center border border-2 border-dark px-1">
+          <div className="col-md-8">
+            <h2>Private Yoga Session</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              tempore.
+            </p>
+          </div>
+          <div className="col-md-2 p-1 image1">
+            <Image
+              src="/acupress.jpeg"
+              alt="hand massage"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="col-md-2">
+          <button>Book Now</button>
+        </div>
+      </div>
+      <div className="d-flex align-items-center px-2 mx-1">
+        <div className="col-md-10 d-flex align-items-center border border-2 border-dark px-1">
+          <div className="col-md-8">
+            <h2>Yoga/self acupressure consultation</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              tempore.
+            </p>
+          </div>
+          <div className="col-md-2 p-1 image1">
+            <Image
+              src="/acupress.jpeg"
+              alt="hand massage"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="col-md-2">
+          <button>Book Now</button>
         </div>
       </div>
       {/* CUPPING, ACCU X CUPPING, PRIVATE YOGA SESSION, YOGA/SELF ACUPRESSURE CONSULTATION */}
